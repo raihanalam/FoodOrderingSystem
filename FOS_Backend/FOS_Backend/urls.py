22bae7d691bcc58f9a13ea805b6454b787f00b75
raihanalam <raihanhridoybd@gmail.com>
2024-02-14 01:16:36 +0600

    New Update

--- FOS_Backend/FOS_Backend/urls.py
+++ FOS_Backend/FOS_Backend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,5 @@ urlpatterns = [
     path('', include('restaurants.urls')),
     # Add other app URLs here
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
